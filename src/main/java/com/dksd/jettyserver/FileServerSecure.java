ae51937e17a531c534a66d383ed366d6ee06be8d
dylansd <dylansd@gmail.com>
2018-05-09 13:24:57 -0700

    Webapp root.

--- src/main/java/com/dksd/jettyserver/FileServerSecure.java
+++ src/main/java/com/dksd/jettyserver/FileServerSecure.java
@@ -16,9 +16,6 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import java.io.*;
 import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 import java.util.Properties;
 
 /**
@@ -26,112 +23,119 @@ import java.util.Properties;
  */
 public class FileServerSecure {
 
-    protected Server server;
+    private static final String MAX_AGE_86400 = "max-age=86400";
+    private Server server;
     private Properties properties;
 
-    public FileServerSecure(Properties properties) throws IOException {
+    private int httpPort;
+    private int httpsPort;
+    private int timeout = 500000;
+    private int bufferSize = 32768;
+    private String keyStorePassword;
+    private String keystorePath;
+    private String webappRoot;
+    private String welcomePage;
+    private String domain;
+
+    public FileServerSecure(Properties properties) {
         this.properties = properties;
+        setProperties(properties);
+    }
+
+    private void setProperties(Properties properties) {
+        httpPort = Integer.parseInt(properties.getProperty("http.port"));
+        httpsPort = Integer.parseInt(properties.getProperty("https.port"));
+        keyStorePassword = properties.getProperty("cert.keystore.password");
+        webappRoot = properties.getProperty("webapp.root");
+        welcomePage = properties.getProperty("welcome.html.page");
+        domain = properties.getProperty("domain");
+        keystorePath = "/etc/letsencrypt/live/" + domain + "/keystore.jks";
     }
 
     public void start() throws Exception {
-        server = new Server(Integer.parseInt(properties.getProperty("http.port")));
-
-        int securePort = -1;
-        try {
-            securePort = Integer.parseInt(properties.getProperty("https.port"));
-        } catch (Exception ep) {
-            ;//no https;
-        }
-        int timeout = 500000;
-        int bufferSize = 32768;
-        int httpPort = Integer.parseInt(properties.getProperty("http.port"));
+        server = new Server(httpPort);
 
         // HTTP Configuration
         // HttpConfiguration is a collection of configuration information appropriate for http and https. The default
         // scheme for http is <code>http</code> of course, as the default for secured http is <code>https</code> but
         // we show setting the scheme to show it can be done.  The port for secured communication is also set here.
-        HttpConfiguration http_config = new HttpConfiguration();
-        http_config.setSecureScheme("https");
-        http_config.setSecurePort(securePort);
-        http_config.setOutputBufferSize(bufferSize);
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSecureScheme("https");
+        httpConfig.setSecurePort(httpsPort);
+        httpConfig.setOutputBufferSize(bufferSize);
 
         // HTTP connector
         // The first server connector we create is the one for http, passing in the http configuration we configured
         // above so it can get things like the output buffer size, etc. We also set the port (8080) and configure an
         // idle timeout.
-        ResourceHandler resource_handler;
+        ResourceHandler resourceHandler;
         GzipHandler gzipHandlerRES;
         HandlerList handlers;
-        try (ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config))) {
+        try (ServerConnector http = new ServerConnector(server,
+                new HttpConnectionFactory(httpConfig))) {
             http.setPort(httpPort);
             http.setIdleTimeout(timeout);
 
-            if (securePort != -1) {
-                // SSL Context Factory for HTTPS and SPDY
-                // SSL requires a certificate so we configure a factory for ssl contents with information pointing to what
-                // keystore the ssl connection needs to know about. Much more configuration is available the ssl context,
-                // including things like choosing the particular certificate out of a keystore to be used.
-                SslContextFactory sslContextFactory = new SslContextFactory();
+            KeyStore.getInstance("JKS").load(new FileInputStream(new File(keystorePath)), keyStorePassword.toCharArray());
+            // SSL Context Factory for HTTPS and SPDY
+            // SSL requires a certificate so we configure a factory for ssl contents with information pointing to what
+            // keystore the ssl connection needs to know about. Much more configuration is available the ssl context,
+            // including things like choosing the particular certificate out of a keystore to be used.
+            SslContextFactory sslContextFactory = new SslContextFactory();
 
-                final String STOREPASSWORD = properties.getProperty("cert.keystore.password");
-                final KeyStore ks = KeyStore.getInstance("JKS");
-                File kf = new File(properties.getProperty("cert.keystore.path"));
-                ks.load(new FileInputStream(kf), STOREPASSWORD.toCharArray());
+            /* I am not sure we need the DST root now with CertBot
+            import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
-                final CertificateFactory cf = CertificateFactory.getInstance("X.509");
-                try (InputStream caInput = new BufferedInputStream(
-                        // this files is shipped with the application
-                        new FileInputStream(properties.getProperty("cert.keystore.dstrootcax3")))) {
-                    final Certificate crt = cf.generateCertificate(caInput);
-                    System.out.println("Added Cert for " + ((X509Certificate) crt)
-                            .getSubjectDN());
-                    ks.setCertificateEntry("DSTRootCAX3", crt);
-                }
+            final CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            try (InputStream caInput = new BufferedInputStream(
+                    // this files is shipped with the application
+                    new FileInputStream(properties.getProperty("cert.keystore.dstrootcax3")))) {
+                final Certificate crt = cf.generateCertificate(caInput);
+                System.out.println("Added Cert for " + ((X509Certificate) crt)
+                        .getSubjectDN());
+                ks.setCertificateEntry("DSTRootCAX3", crt);
+            }*/
 
-                sslContextFactory.setKeyStorePath(properties.getProperty("cert.keystore.path"));
-                sslContextFactory.setKeyStorePassword(properties.getProperty("cert.keystore.password"));
-                sslContextFactory.setKeyManagerPassword(properties.getProperty("cert.keystore.password"));
+            sslContextFactory.setKeyStorePath(keystorePath);
+            sslContextFactory.setKeyStorePassword(keyStorePassword);
+            sslContextFactory.setKeyManagerPassword(keyStorePassword);
 
-                // HTTPS Configuration
-                // A new HttpConfiguration object is needed for the next connector and you can pass the old one as an
-                // argument to effectively clone the contents. On this HttpConfiguration object we add a
-                // SecureRequestCustomizer which is how a new connector is able to resolve the https connection before
-                // handing control over to the Jetty Server.
-                HttpConfiguration https_config = new HttpConfiguration(http_config);
-                https_config.addCustomizer(new SecureRequestCustomizer());
+            // HTTPS Configuration
+            // A new HttpConfiguration object is needed for the next connector and you can pass the old one as an
+            // argument to effectively clone the contents. On this HttpConfiguration object we add a
+            // SecureRequestCustomizer which is how a new connector is able to resolve the https connection before
+            // handing control over to the Jetty Server.
+            HttpConfiguration httpsConfig = new HttpConfiguration(httpConfig);
+            httpsConfig.addCustomizer(new SecureRequestCustomizer());
 
-                // HTTPS connector
-                // We create a second ServerConnector, passing in the http configuration we just made along with the
-                // previously created ssl context factory. Next we set the port and a longer idle timeout.
-                try (ServerConnector https = new ServerConnector(server,
-                        new SslConnectionFactory(sslContextFactory, "http/1.1"),
-                        new HttpConnectionFactory(https_config))) {
-                    https.setPort(securePort);
-                    https.setIdleTimeout(timeout);
-                    server.setConnectors(new Connector[]{http, https});
-                }
-            } else {
-                server.setConnectors(new Connector[]{http});
+            // HTTPS connector
+            // We create a second ServerConnector, passing in the http configuration we just made along with the
+            // previously created ssl context factory. Next we set the port and a longer idle timeout.
+            try (ServerConnector https = new ServerConnector(server,
+                    new SslConnectionFactory(sslContextFactory, "http/1.1"),
+                    new HttpConnectionFactory(httpsConfig))) {
+                https.setPort(httpsPort);
+                https.setIdleTimeout(timeout);
+                server.setConnectors(new Connector[]{http, https});
             }
         }
 
-        resource_handler = new ResourceHandler();
-        resource_handler.setWelcomeFiles(new String[]{ properties.getProperty("welcome.html.page") });
-        //TODO externalize this to point elsewhere, aka not in the code FFS
-        resource_handler.setResourceBase(properties.getProperty("resource.base"));
-        resource_handler.setCacheControl("max-age=86400");
-        resource_handler.setEtags(true);
+        resourceHandler = new ResourceHandler();
+        resourceHandler.setWelcomeFiles(new String[]{welcomePage});
+        resourceHandler.setResourceBase(webappRoot);
+        resourceHandler.setCacheControl(MAX_AGE_86400);
+        resourceHandler.setEtags(true);
 
         gzipHandlerRES = new GzipHandler();
         gzipHandlerRES.setIncludedMimeTypes("text/html,text/plain,text/xml,text/css,application/javascript,text/javascript");
-        gzipHandlerRES.setHandler(resource_handler);
+        gzipHandlerRES.setHandler(resourceHandler);
 
         handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{gzipHandlerRES, new DefaultHandler()});
         server.setHandler(handlers);
 
-        //server.setHandler(new HelloHandler());
-        // Start the server
         server.start();
         server.join();
     }
